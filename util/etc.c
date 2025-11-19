8b3ee1985305737378bebf7d8d0f10c1e5ccd7b3
vboxuser <lekimpel@gmail.com>
2025-11-19 12:17:17 +0000

    comment formatting

--- util/etc.c
+++ util/etc.c
@@ -6,7 +6,9 @@
 #include <dlfcn.h> // for loading reasons
 #include "etc.h"
 
-/* Note it's probably more efficient and reasonable to do this in Python. However my C skills are rusty and I need to keep in shape. Parses /proc/ pseudofilesystem for the PID associated with a particular process name. */
+/* Note it's probably more efficient and reasonable to do this in Python.
+ However my C skills are rusty and I need to keep in shape.
+Parses /proc/ pseudofilesystem for the PID associated with a particular process name. */
 pid_t loc_process(char *target)
   {
     if (target == NULL)
